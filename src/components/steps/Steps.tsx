621fd4e444140ad1b6c293e4bcc0748dcb44f868
mishota <mishota.o@gmail.com>
2023-01-23 19:33:25 +0300

    fonts

--- src/components/steps/Steps.tsx
+++ src/components/steps/Steps.tsx
@@ -13,18 +13,12 @@ import vue from "@/../public/vue.png";
 import arrowL from "@/../public/arrowL.png";
 
 const inter = Inter({ subsets: ["latin"] });
-const items = [
+const itemsL = [
   {
     id: "Step 1",
     title: "Introduction to Front-End",
     text: "Lorem ipsum dolor sit amet consectetur. Elit massa erat vitae non semper quis. ",
     rotate: 0,
-  },
-  {
-    id: "Step 2",
-    title: "Overview of Development",
-    text: "Lorem ipsum dolor sit amet consectetur. Elit massa erat vitae non semper quis. ",
-    rotate: 180,
   },
   {
     id: "Step 3",
@@ -33,16 +27,24 @@ const items = [
     rotate: 0,
   },
   {
-    id: "Step 4",
+    id: "Step 5",
+    title: "Introduction to Front-End",
+    text: "Lorem ipsum dolor sit amet consectetur. Elit massa erat vitae non semper quis. ",
+    rotate: 0,
+  },
+];
+const itemsR = [
+  {
+    id: "Step 2",
     title: "Overview of Development",
     text: "Lorem ipsum dolor sit amet consectetur. Elit massa erat vitae non semper quis. ",
     rotate: 180,
   },
   {
-    id: "Step 5",
-    title: "Introduction to Front-End",
+    id: "Step 4",
+    title: "Overview of Development",
     text: "Lorem ipsum dolor sit amet consectetur. Elit massa erat vitae non semper quis. ",
-    rotate: 0,
+    rotate: 180,
   },
   {
     id: "Step 6",
@@ -58,8 +60,8 @@ export default function Steps() {
       <div className={styles.title}> Steps</div>
 
       <div className={styles.content_container}>
-        <div className={styles.side}>
-          {items.map((elem) => (
+        <div className={styles.side_up}>
+          {itemsL.map((elem) => (
             <div key={elem.id} className={styles.box_arrow_container}>
               <div className={styles.box_container}>
                 <div className={styles.half_linear} />
@@ -79,8 +81,8 @@ export default function Steps() {
           ))}
         </div>
         <div className={styles.center_line} />
-        <div className={styles.side}>
-          {items.map((elem) => (
+        <div className={styles.side_down}>
+          {itemsR.map((elem) => (
             <div
               key={elem.id}
               className={styles.box_arrow_container}
@@ -88,7 +90,10 @@ export default function Steps() {
             >
               <div className={styles.box_container}>
                 <div className={styles.half_linear} />
-                <div className={styles.main_box} style={{ transform: "rotate(180deg)" }}>
+                <div
+                  className={styles.main_box}
+                  style={{ transform: "rotate(180deg)" }}
+                >
                   <div className={styles.step_number}>{elem.id}</div>
                   <div className={styles.step_title}>{elem.title}</div>
                   <div className={styles.step_text}>{elem.text}</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.scss";
 import Main from "@/components/Main/Main";
 import Header from "@/components/Header/Header";
 import AboutUs from "@/components/AboutUs/AboutUs";
 import Technologies from "@/components/Technologies/Technologies";
 import Steps from "@/components/steps/Steps";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -23,7 +20,7 @@ export default function Home() {
         <Main />
         <AboutUs />
         <Technologies />
-        <Steps/>
+        <Steps />
       </div>
     </>
   );
